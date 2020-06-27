5462342f715b9691b11f4bc9562d0375c1f00272
paulor8bit <thunder_momon@hotmail.com>
2020-06-27 11:23:12 -0300

    Implementação do backend para aplicação online

---
+++ tasksBackend/knexfile.js
@@ -0,0 +1,31 @@
+const { db } = require('./.env')
+module.exports = {
+    client: 'postgresql',
+    connection: db,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  };
+
+
+
+// module.exports = {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'tasks',
+//       user:     'username',
+//       password: 'docker'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
